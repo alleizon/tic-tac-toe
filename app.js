467bd1900f06b04eb9f48596098242875c839ef4
alleizon <112495304+alleizon@users.noreply.github.com>
2023-01-18 18:22:45 +0200

    refactored Gameboard & displayBoard + added EL for updating the board

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 const Gameboard = (() => {
   const gameboard = [
-    ["X", "X", "O"],
-    ["O", "X", "O"],
-    ["O", "O", "X"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
 
-  const displayBoard = () => {
-    const boardWrapper = document.createElement("div");
-    boardWrapper.classList.add("board-container");
-    gameboard.forEach((row) => {
-      row.forEach((col) => {
-        const div = document.createElement("div");
-        div.textContent = col;
-        boardWrapper.appendChild(div);
-      });
-    });
-    const body = document.querySelector("body");
-    body.appendChild(boardWrapper);
+  const updateBoard = (x, y, symbol) => {
+    gameboard[x][y] = symbol;
+    console.log(gameboard);
   };
 
+  return { updateBoard };
+})();
+
+const displayController = (() => {
+  const body = document.querySelector("body");
+  const div = document.createElement("div");
+  div.classList.add("board-container");
+  body.appendChild(div);
+
+  const displayBoard = () => {
+    for (let i = 0; i < 3; i += 1) {
+      for (let j = 0; j < 3; j += 1) {
+        const btn = document.createElement("button");
+        btn.dataset.x = i;
+        btn.dataset.y = j;
+        btn.addEventListener("click", player1.move);
+        btn.setAttribute("type", "button");
+        div.appendChild(btn);
+      }
+    }
+  };
   return { displayBoard };
 })();
 
-Gameboard.displayBoard();
+function Player(name, symbol) {
+  const move = (e) => {
+    const { x } = e.target.dataset;
+    const { y } = e.target.dataset;
+    Gameboard.updateBoard(+x, +y, symbol);
+  };
+
+  return { name, symbol, move };
+}
+
+const player1 = Player(1, "X");
+const player2 = Player(2, "O");
+displayController.displayBoard();
